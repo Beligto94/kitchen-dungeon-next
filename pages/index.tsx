d9eb51b7030d697e3c0eeeda62b1534b7553f6e5
Dampilov Beligto <dampilov.beligto@gmail.com>
2022-06-16 16:01:17 +0800

    #1 first

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,10 @@ export default function Home() {
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quidem ve
       repellat magnam perspiciatis magni laborum, assumenda facilis, doloribus
       pariatur sit ducimus, corporis autem similique veritatis explicabo
-      doloremque corrupti itaque.
+      doloremque corrupti itaque. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Doloremque maxime asperiores consectetur nihil, debitis
+      incidunt ratione necessitatibus quisquam rem illum neque quaerat
+      voluptatum accusantium assumenda tenetur ea cum ut minima!
     </div>
   );
 }
