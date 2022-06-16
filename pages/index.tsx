e5dbf6f56949b3499ffd7b059bc2cc072ea53019
Dampilov Beligto <dampilov.beligto@gmail.com>
2022-06-16 15:43:53 +0800

    sdf

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quidem ve
+      repellat magnam perspiciatis magni laborum, assumenda facilis, doloribus
+      pariatur sit ducimus, corporis autem similique veritatis explicabo
+      doloremque corrupti itaque.
+    </div>
+  );
+}
